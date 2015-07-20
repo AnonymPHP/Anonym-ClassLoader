7f0579c839b263f73ab8697b0df3eb2f37205cee
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-21 01:03:13 +0300

    first update

---
+++ src/Anonym/Components/ClassLoader.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\ClassLoader;
+    /**
+     * Class ClassLoader
+     * @package Anonym\Components\ClassLoader
+     */
+    class ClassLoader
+    {
+
+    }
