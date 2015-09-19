aa31ec377b2c7157aa18ee9f0643e54569d22860
anonymframework <serif.saglam119@outlook.com>
2015-09-19 13:48:36 +0300

    made some updates

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
 <?php
 
-    include 'vendor/autoload.php';
-    ini_set('display_errors', 'On');
-    $loader = new \Anonym\Components\ClassLoader\Psr4Finder();
-
-    $loader->findFile('Foo\Bar\Baz\Dib\Zim\Gir\ClassName');
-
--- loader.php
+++ loader.php
@@ -8,4 +8,34 @@
  * Thanks for using
  */
 
+use Anonym\Components\ClassLoader\ClassLoader;
+
+if (!defined('BASE')) {
+    define('BASE', getcwd() . '/');
+}
+
 include 'vendor/composer/ClassLoader.php';
+
+use Composer\Autoload\ClassLoader as Compoer;
+
+$composer = new Compoer();
+
+$map = require BASE . 'vendor/composer/autoload_namespaces.php';
+foreach ($map as $namespace => $path) {
+    $composer->set($namespace, $path);
+}
+
+$map = require BASE . 'vendor/composer/autoload_psr4.php';
+foreach ($map as $namespace => $path) {
+    $composer->setPsr4($namespace, $path);
+}
+
+$classMap = require BASE . 'vendor/composer/autoload_classmap.php';
+if ($classMap) {
+    $composer->addClassMap($classMap);
+}
+
+$composer->register(true);
+
+// create anonym class loader instance
+return new ClassLoader($composer);
